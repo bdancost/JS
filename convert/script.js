1f32f4da8a1374f7aa6fe3d25ab6ef2d0791d084
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-09-27 21:05:51 -0300

    Adicionando cases nocodigo

--- convert/script.js
+++ convert/script.js
@@ -1,3 +1,9 @@
+// Cotaao demoeda do dia
+const USD = 5.4
+const EUR = 6.2
+const GBP = 7.2
+
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
 const currency = document.getElementById('currency')
 const result = document.getElementById('result')
@@ -7,3 +13,25 @@ amount.addEventListener('input', (event) => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, '')
 })
+
+//Capturando o evento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value) {
+    case 'USD':
+      convertCurrency(amount.value, USD, 'US$')
+      break
+    case 'EUR':
+      convertCurrency(amount.value, EUR, '€')
+      break
+    case 'GBP':
+      convertCurrency(amount.value, GBP, '£')
+      break
+    case 'BRL':
+      convertCurrency(amount.value, 1, 'BRL')
+  }
+
+  //Função de conversão
+  function convertCurrency(amount, price, symbol) {}
+}
