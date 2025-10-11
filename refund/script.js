b54205dbdf18c2e95be3e8badeb40937a8863530
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-10-11 10:41:13 -0300

    Criando o valor da despesa

--- refund/script.js
+++ refund/script.js
@@ -78,8 +78,13 @@ function expenseAdd(newExpense) {
     // Adiciona o nome e a categoria na div de info das despesas
     expenseInfo.append(expenseName, expenseCategory)
 
+    // Cria o valor da despesa
+    const expenseAmount = document.createElement('span')
+    expenseAmount.classList.add('expense-amount')
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace('R$', '')}`
+
     // Adiciona as informaoes no item
-    expenseItem.append(expenseIcon, expenseInfo)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
     // Adiciona o item na lista
     expenseList.append(expenseItem)
   } catch (error) {
